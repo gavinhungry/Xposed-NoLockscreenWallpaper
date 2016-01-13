e4eab2d88034cbb32ce2152bc7ba47702a0b2fb1
Gavin Lloyd <gavinhungry@gmail.com>
2016-01-13 14:44:02 -0800

    Cleanup

--- src/io/gavinhungry/xposed/nolockscreenwallpaper/NoLockscreenWallpaper.java
+++ src/io/gavinhungry/xposed/nolockscreenwallpaper/NoLockscreenWallpaper.java
@@ -8,17 +8,13 @@ import de.robv.android.xposed.XC_MethodReplacement;
 
 public class NoLockscreenWallpaper implements IXposedHookLoadPackage {
   public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-    if (lpparam.packageName.equals("com.android.systemui")) {
-      try {
-
-        final Class<?> PhoneStatusBar = XposedHelpers.findClass("com.android.systemui.statusbar.phone.PhoneStatusBar",
-          lpparam.classLoader);
-
-        XposedBridge.hookAllMethods(PhoneStatusBar, "updateMediaMetaData", XC_MethodReplacement.DO_NOTHING);
-
-      } catch(Throwable t) {
-        XposedBridge.log(t);
-      }
+    if (!lpparam.packageName.equals("com.android.systemui")) {
+      return;
     }
+
+    final Class<?> PhoneStatusBar = XposedHelpers.findClass("com.android.systemui.statusbar.phone.PhoneStatusBar",
+      lpparam.classLoader);
+
+    XposedBridge.hookAllMethods(PhoneStatusBar, "updateMediaMetaData", XC_MethodReplacement.DO_NOTHING);
   }
 }
